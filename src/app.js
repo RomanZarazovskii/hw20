89e27c6f52bb8ec16de7350e6ed39469ba082212
RomanZarazovskii <124078828+RomanZarazovskii@users.noreply.github.com>
2024-12-10 19:25:53 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const API_KEY = '47286351-e716f5558799522fb527bbff6';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export default {
-  query: '',
-  page: 1,
+export default class App {
+  constructor() {
+    query: '';
+    page: 1;
+  }
   async fetchImages() {
     const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${API_KEY}`;
     try {
@@ -18,8 +20,8 @@ export default {
       console.log('Error');
       throw error;
     }
-  },
+  }
   resetPage() {
     this.page = 1;
-  },
-};
+  }
+}
